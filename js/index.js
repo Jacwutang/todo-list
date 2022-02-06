701ebf2f87ba1f7a46ebfbb4c206132481aacaa0
Jack Wu <jack@Jacks-MacBook-Pro.local>
2022-02-06 13:29:26 -0500

    remove logic to set id | refactor to es6: arrow functions

--- js/index.js
+++ js/index.js
@@ -1,13 +1,10 @@
-var liElementCounter = 0;
-
-function myFunction(event){
+const myFunction = event => {
     event.preventDefault()
 
     let text  = event.target['submit-item'].value
 
     let li = document.createElement('li')
-    li.id = liElementCounter++;
-
+    
     let li_text = document.createTextNode(text)
     li.appendChild(li_text)
 
@@ -18,18 +15,15 @@ function myFunction(event){
     
     let ul = document.getElementById('ul')
     ul.appendChild(li)
-
 }
 
-function onChange(event) {
+const onChange = event => {
     if (event.target.type === "checkbox" && event.target.checked === true) {
         
         event.target.parentElement.remove()
     }
-    
 }
 
 document.addEventListener("submit", myFunction);
 
 document.addEventListener("change", onChange);
-
