0ad68fb3df7b3a6044a066f8e770b48771d34038
Jack Wu <jack@jacks-mbp.lan>
2022-02-08 19:38:17 -0500

    add deletion support

--- todo-list/src/todoApp.js
+++ todo-list/src/todoApp.js
@@ -4,7 +4,7 @@ class TodoApp extends react.Component {
     constructor(props) {
         super(props);
 
-        this.state = {'items': [], 'currentItem': ''};
+        this.state = {'items': [], 'newItem': ''};
 
         this.handleSubmit = this.handleSubmit.bind(this);
         this.handleChange = this.handleChange.bind(this);
@@ -12,29 +12,45 @@ class TodoApp extends react.Component {
 
     handleSubmit(e) {
         e.preventDefault();
-        const newTodoItem = this.state.currentItem;
+        const newTodoItem = this.state.newItem;
         this.setState({'items': this.state.items.concat(newTodoItem)})
     }
 
     handleChange(e) {
         e.preventDefault();
-        this.setState({'currentItem': e.target.value});
+        
+        if(e.target.type === "checkbox") {
+            const itemsIdx = parseInt(e.target.value);
+            
+            this.setState(
+                {
+                    'items': this.state.items.filter( (item,idx) => idx !== itemsIdx)
+                }
+            );
+        }
+        else {
+            this.setState({'newItem': e.target.value});
+        }
     }
-
+    
     render() {
         return (
             <div>
                 <form onSubmit={this.handleSubmit}>
                     <label> 
                         Add Todo:
-                        <input type="text" value={this.state.currentItem} onChange={this.handleChange}/>
+                        <input type="text" value={this.state.newItem} onChange={this.handleChange}/>
                     </label>
                     <input type="submit" value="Submit"/>
                 </form>
             
                 {this.state.items.map( (item, idx) => 
                     <li key={idx}>
-                        {item}
+                        <div> 
+                            <h2> {item} 
+                                <input type="checkbox" value={idx} onChange={this.handleChange}/> 
+                            </h2>
+                        </div>
                     </li>
                 )}
             </div>
